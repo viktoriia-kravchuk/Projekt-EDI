2c88f4d658de2a650fe0fab995edc4bf64208756
viktoriia-kravchuk <vikakravchuk2@gmail.com>
2020-01-18 13:57:15 +0100

    last

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ function connect(){
     var method="GET";
     var URL="http://manishtamaria.com/storerental/api/v1/books";
     var request= new XMLHttpRequest;
-    request.onreadystatechange=function(){display(this)}
     request.open(method,URL);
+    request.onreadystatechange=function(){display(this)}
     request.send();
 }
 function display(x){
@@ -12,40 +12,50 @@ function display(x){
         var data=x.responseText;
         var books=JSON.parse(data);
         for (var i=0;i<20;i++){
-            var div1=document.createElement('div');
-            div1.className="card";
-            div1.style.width="18em";
-            div1.style.height="25em";
-                var pic=document.createElement("img");
-                pic.className="card-img-top";
-                pic.src=books[i].image.imageUrl;
-                div1.appendChild(pic);
-                var div2=document.createElement("div");
-                div2.className="card-img-overlay";
-                div2.style.visibility="hidden";
-                var text=document.createElement("p");
-                text.className="card-text"
-                text.innerHTML=books[i].description;
-                div2.appendChild(text);
-                document.getElementById("container").appendChild(div1);  
+            var parentDiv=document.createElement('div');
+            var childDiv=document.createElement("div");
+            var thumbnail=document.createElement("img");
+            var description=document.createElement("p");
+
+            parentDiv.className="card";
+            parentDiv.style.width="18em";
+            parentDiv.style.height="25em";
+           
+            thumbnail.className="card-img-top";
+            thumbnail.src=books[i].image.imageUrl;
+
+            parentDiv.appendChild(thumbnail);
+           
+            childDiv.className="card-img-overlay";
+            childDiv.style.visibility="hidden";
+            
+            description.className="card-text"
+            description.innerHTML=books[i].description;
+
+            childDiv.appendChild(description);
+            parentDiv.append(childDiv)
+
+            document.getElementById("container").appendChild(parentDiv);  
         }
+        console.log("all done")
 
         
     }
     $(".card").hover( 
         function(){
             var $this = $(this);
-            $($this[0].lastElementChild).css("visibility","visible")
-            $($this[0].lastElementChild).css("background","rgba(0,0,0,0.7)")
-            $($this[0].lastElementChild).css("color","white")
-            $($this[0].lastElementChild).css("font-size","small")
-            $(".card").append(div2);
+            var currentElement = $this[0].lastElementChild;
+            $(currentElement).css("visibility","visible")
+            $(currentElement).css("background","rgba(0,0,0,0.7)")
+            $(currentElement).css("color","white")
+            $(currentElement).css("font-size","small")
+            
     },
     function()
     {
         var $this = $(this);
         $($this[0].lastElementChild).css("visibility","hidden")
-        $(".card").append(div2);
+       
     });
     }
 
